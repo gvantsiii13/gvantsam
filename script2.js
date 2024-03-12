2b28b7a42303d998d3afe6bf5f9a6bf76e3f38a3
gvantsiii13 <gmamuchashvili13@gmail.com>
2024-03-12 20:02:53 +0400

    add new homework2

---
+++ script2.js
@@ -0,0 +1,20 @@
+let a=87; b=65;
+console.log(a+b);
+let student = {
+    firstname: "saba",
+    lastname: "sabanaZe",
+    age: 29,
+    email: "sabanadze#gmail.com",
+    marks: 1
+}
+console.log(student.firstname);
+//გამოაცხადეთ ორი ცვლადი m და n მნიშვნელობებით 45 და 23,
+// დაბეჭდეთ მათი ჯამი თუ m მეტია n ზე , თუ არადა დაბეჭდეთ n
+
+let m=45; n=23;
+if (m > n ) {
+console.log(m+n)
+}
+else {
+    console.log(n)
+};
